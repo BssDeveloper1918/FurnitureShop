9d20194675b47032039460966411a38721dd6e19
BssDeveloper1918 <ion.ivasco@gmail.com>
2023-01-24 21:23:44 -0800

    added index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
+// use strict
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
 $(function(){
-    
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  }); 
+  $(function(){
+    var mixer = mixitup('.products__inner');
+  }
+  )
+  $(function(){
+    var mixer = mixitup('.design__inner');
+  }
+  )
 })
--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,6 @@ function nunjucks(){
 function styles(){
     return src('app/scss/*.scss')
     .pipe(scss({outputStyle:'compressed'}))
-    // .pipe(concat())
     .pipe(rename({
         suffix : '.min'
     }))
@@ -78,6 +77,7 @@ function scripts(){
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
         'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'node_modules/mixitup/dist/mixitup.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
