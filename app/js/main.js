9e1a5c3fdcfbcc06b82cb04bcbe84d63862b6831
BssDeveloper1918 <ion.ivasco@gmail.com>
2023-01-26 20:18:03 -0800

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -22,4 +22,29 @@ $(function(){
     var mixer = mixitup('.design__inner');
   }
   )
+  $(function(){
+    
+    $(".filter-price__input").ionRangeSlider({
+      type: "double",
+      prefix: "$",
+      onStart: function (data) {
+        $('.filter-price__from').text(data.from);
+       $('.filter-price__to').text(data.to);
+      },
+      onChange: function (data) {
+       $('.filter-price__from').text(data.from);
+       $('.filter-price__to').text(data.to);
+     },
+    })
+  })
+  
+  $(function () {
+    $(".products-star").rateYo({
+      starWidth: "17px",
+      normalFill: "#ccccce",
+      ratedFill: "#ffc35b",
+      readOnly: true
+    });
+   });
+ 
 })
